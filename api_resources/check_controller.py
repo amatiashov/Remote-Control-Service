1b2828289b4ae8fbb389fed743440119016a478a
Artem Matiashov <matiashov.artem@gmail.com>
2021-05-11 12:23:58 +0300

    Send default value if exception is occurred

--- api_resources/check_controller.py
+++ api_resources/check_controller.py
@@ -10,11 +10,19 @@ RESOURCES_DIR = os.path.join(BASE_DIR, "resources")
 
 class CheckController(Resource):
     _logger = None
+    _default_response = None
 
     def __init__(self):
         self._logger = logging.getLogger(__name__)
+        self._default_response = 0
 
     def get(self):
-        with open(os.path.join(RESOURCES_DIR, "response.json"), encoding="utf-8") as f:
-            response_template = json.loads(f.read())
-            return response_template.get("data")
+        try:
+            with open(os.path.join(RESOURCES_DIR, "response.json"), encoding="utf-8") as f:
+                response_template = json.loads(f.read())
+                if not response_template.get("data"):
+                    raise RuntimeError("Response template does not contain the 'data' field!")
+                return response_template.get("data")
+        except Exception as e:
+            self._logger.critical("Cannot send data from file: %s" % str(e))
+            return self._default_response
